339d443efb68483c7e0e3383b1bfd732bba270c8
Carl Funk <carlalanfunk@outlook.com>
2022-02-15 22:11:51 -0600

    Initial Commit

---
+++ WeatherUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherUI
+//
+//  Created by Carl Funk on 2/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherUI/WeatherUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherUIApp.swift
+//  WeatherUI
+//
+//  Created by Carl Funk on 2/15/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
